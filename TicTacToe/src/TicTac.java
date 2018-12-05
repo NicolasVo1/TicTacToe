14fe8941c5895c36d159c3a95aa4aa45e3bdc513
NicolasVo1 <45612532+NicolasVo1@users.noreply.github.com>
2018-12-04 18:03:52 -0800

    initial

---
+++ TicTacToe/src/TicTac.java
@@ -0,0 +1,13 @@
+
+public class TicTac {
+	public static void main(String [] args) {
+		int [][] grId= {{1,2,3},{4,5,6},{7,8,9}};
+		for (int i=0; i<3; i++) {
+			for (int a=0; a<3; a++) {
+				System.out.print(grId[i][a]);
+			}
+			System.out.println("");
+		}
+	}
+
+}
